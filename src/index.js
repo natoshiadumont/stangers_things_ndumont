9f25bb08d24dc54147de0c24ee648f8c4915e397
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-12-12 20:21:33 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  ReactDOM  from 'react';
+
+const App = () => {
+  return (
+  <div>
+    Hello World!
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+)
