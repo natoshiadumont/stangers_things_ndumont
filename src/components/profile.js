d6964c9d5c8fab0d48d385dae253110ff7a0cde9
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-12-31 00:13:23 -0600

    fixed messages bug

--- src/components/profile.js
+++ src/components/profile.js
@@ -18,6 +18,21 @@ export const Profile = ({ authenticated }) => {
       <h3>Profile</h3>
       {authenticated ?
         <>
+                  <div id="recieved">Recieved Messages:</div>
+          {messages.map((message, index) => {
+              if (message.fromUser.username !== user) {
+                return (
+                
+                    <div key={index} className="messages-container">
+
+                      <div id="message-title">Message related to "{message.post.title}" post:</div>
+                      <div id="message-content">You sent: "{message.content}"</div>
+
+                    </div> 
+              )
+            }
+            }
+          )}
           <div id="sent">Sent Messages:
             {messages.map((message, index) => {
               if (message.fromUser.username === user) {
@@ -32,8 +47,8 @@ export const Profile = ({ authenticated }) => {
               )
             }
             }
+          )}
           </div>
-          <div id="recieved">Recieved Messages:</div>
         </>
         : <div>You must be logged into an account to view profile details.</div>}
     </div>
